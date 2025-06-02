417da0ca99a412043db6db548beba9ae13274017
Farik on Security <farikonsec@gmail.com>
2025-06-02 15:30:44 +0100

    Create main.go
    Main function to get weather

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+)
+
+func main() {
+	resp, err := http.Get("https://wttr.in/London?format=3")
+	if err != nil {
+		fmt.Println("Error fetching weather:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("🌦️ London Weather:", string(body))
+}
